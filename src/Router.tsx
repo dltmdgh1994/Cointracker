0711b1c9e988cde3409497c06652ca9ec5558e73
Seungho Lee <60536427+dltmdgh1994@users.noreply.github.com>
2022-03-18 15:43:16 +0900

    Create Cointracker

---
+++ src/Router.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Chart from "./routes/Chart";
+import Coin from "./routes/Coin";
+import Coins from "./routes/Coins";
+import Price from "./routes/Price";
+
+function Router() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Coins />} />
+        {/* nested routes */}
+        <Route path="/:coinId" element={<Coin />}>
+          <Route path="chart" element={<Chart />} />
+          <Route path="price" element={<Price />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
